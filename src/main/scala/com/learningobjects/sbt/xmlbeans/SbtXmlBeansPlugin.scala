140e593bc2766a8f52d32aad97038c85c42cb78d
Zack Powers <zpowers@learningobjects.com>
2015-06-22 01:05:29 -0400

    Fix dependency classpath

--- src/main/scala/com/learningobjects/sbt/xmlbeans/SbtXmlBeansPlugin.scala
+++ src/main/scala/com/learningobjects/sbt/xmlbeans/SbtXmlBeansPlugin.scala
@@ -24,12 +24,8 @@ object SbtXmlBeansPlugin extends AutoPlugin {
     //TODO: Enable use to define the which version of xmlbeans to run by using a forked process.
     //val xmlbeansLibs = SettingKey[Seq[ModuleID]]("xmlbeans-libs", "The apache xmlbeans module this project will use.")
     //val xmlbeansCmd = SettingKey[Boolean]("xmlbeans-cmd", "The command line arguments used to launch the xmlbeans compiler.")
-    val xmlbeansDebug = SettingKey[Boolean]("xmlbeans-debug", "Enabled debug output.")
-    val xmlbeansVerbose = SettingKey[Boolean]("xmlbeans-verbose", "Enabled verbose output.")
-    val xmlbeansQuiet = SettingKey[Boolean]("xmlbeans-quiet", "Enabled quiet output.")
-    val xmlbeansSourceVersion = SettingKey[String]("xmlbeans-source-version", "Java source level for generated xmlbean sources.")
+    val xmlbeansParams = SettingKey[XmlbeansParams]("xmlbeans-params", "Parameters for the xmlbeans schema compiler")
   }
-
   import autoImport._
 
   val XmlBeans = config("xmlbeans").hide
@@ -39,30 +35,27 @@ object SbtXmlBeansPlugin extends AutoPlugin {
 //    xmlbeansLibs := Seq(
 //      "org.apache.xmlbeans" % "xmlbeans" % "2.4.0"
 //    ),
-    xmlbeansDebug := false,
-    xmlbeansVerbose := false,
-    xmlbeansQuiet := true,
-    xmlbeansSourceVersion := "1.5"
+    xmlbeansParams := XmlbeansParams(debug = true, verbose = false, quiet = true, download = false, javaSource = "1.5")
     //libraryDependencies <++= (xmlbeansLibs)(_.map(_ % XmlBeans.name))
   ) ++ inConfig(Compile)(unscopedSettings)
 
   private def unscopedSettings = Seq[Def.Setting[_]](
-    xmlbeans <<= (sourceDirectory, sourceManaged, classDirectory, internalDependencyClasspath,
-      xmlbeansDebug, xmlbeansVerbose, xmlbeansQuiet, xmlbeansSourceVersion, streams) map {
+    xmlbeans <<= (sourceDirectory in Compile, sourceManaged in Compile, classDirectory, dependencyClasspath in Compile,
+      xmlbeansParams, streams) map {
       cachedGenerateSources
     },
     sourceGenerators <+= xmlbeans
   )
 
-  def cachedGenerateSources(sourceDir: File, srcOut: File, classOut: File, classpath: Classpath, debug: Boolean, verbose: Boolean, quiet: Boolean, javaSource: String, streams: TaskStreams): Seq[File] = {
+  def cachedGenerateSources(sourceDir: File, srcOut: File, classOut: File, classpath: Classpath, xmlbeansParams: XmlbeansParams, streams: TaskStreams): Seq[File] = {
     FileFunction.cached(streams.cacheDirectory)(FilesInfo.lastModified, FilesInfo.exists) {
-      (inputFiles, modifiedOutputFile) => generateSources(inputFiles, sourceDir, srcOut, classOut, classpath, debug, verbose, quiet,javaSource).toSet
+      (inputFiles, modifiedOutputFile) => generateSources(inputFiles, sourceDir, srcOut, classOut, classpath, xmlbeansParams).toSet
     } {
       ((sourceDir ** "*.xsd") +++ (sourceDir ** "*.xsdconfig") +++ (sourceDir ** "*.java")).get.toSet
     }.toSeq //TODO: toSet/toSeq conversions are awkward.
   }
 
-  def generateSources(sourceReport: ChangeReport[File], sourceDir: File, srcOut: File, classOut: File, classpath: Classpath, debug: Boolean, verbose: Boolean, quiet: Boolean, javaSource: String): Seq[File] = {
+  def generateSources(sourceReport: ChangeReport[File], sourceDir: File, srcOut: File, classOut: File, classpath: Classpath, xmlbeansParams: XmlbeansParams): Seq[File] = {
     sourceDir.mkdirs()
     srcOut.mkdirs()
 
@@ -71,13 +64,13 @@ object SbtXmlBeansPlugin extends AutoPlugin {
     val xsds = (sourceDir / "xsd") ** "*.xsd"
     val xsdconfigs = (sourceDir / "xsdconfig") ** "*.xsdconfig"
 
+    println(xsdconfigs.getPaths)
+
     val javaInput = (sourceDir / "java") ** "*.java"
 
-    val cp = (classpath map {
-      _.data
-    }).toArray
+    val cp = classpath.files.toArray
 
-    val errorListener = new XmlErrorPrinter(verbose, classOut.toURI)
+    val errorListener = new XmlErrorPrinter(xmlbeansParams.verbose, classOut.toURI)
 
     params setXsdFiles xsds.get.toArray
     params setWsdlFiles Array.empty[File]
@@ -86,12 +79,12 @@ object SbtXmlBeansPlugin extends AutoPlugin {
     params setClasspath cp
     params setSrcDir srcOut
     params setClassesDir classOut
-    params setJavaSource javaSource
+    params setJavaSource xmlbeansParams.javaSource
     params setNojavac true
-    params setDebug debug
-    params setVerbose verbose
-    params setQuiet quiet
-    params setDownload false
+    params setDebug xmlbeansParams.debug
+    params setVerbose xmlbeansParams.verbose
+    params setQuiet xmlbeansParams.quiet
+    params setDownload xmlbeansParams.download
     params setNoVDoc false
     params setNoUpa false
     params setNoPvr false
@@ -103,6 +96,8 @@ object SbtXmlBeansPlugin extends AutoPlugin {
     if (success)
       srcOut ** "*.java" get
     else
-      Seq.empty[File]
+      throw new RuntimeException("XSD Compilation failed.")
   }
+
+  case class XmlbeansParams(debug: Boolean, verbose: Boolean, quiet: Boolean, download: Boolean, javaSource: String)
 }
